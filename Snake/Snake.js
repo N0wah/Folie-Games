e461bd038b7ebab0d03a6041c424c81dfcfcefaf
AlongkornFR <144777531+AlongkornFR@users.noreply.github.com>
2023-11-23 10:09:14 +0100

    html snake ajout

--- Snake/Snake.js
+++ Snake/Snake.js
@@ -1,55 +1,65 @@
 "use strict";
-const playBoard = document.getElementById("play-board");
-const scoreElement = document.getElementById("Score");
-const highScoreElement = document.getElementById("hScore");
+const playBoard = document.querySelector(".play-board"); //constant qui recupere "playBoard"
+const scoreElement = document.querySelector(".score"); //constant qui recupere "score"
+const highScoreElement = document.querySelector(".hScore");//constant qui recupere "hScore"
 
-let foodX, foodY;
-let snakeX = 6, snakeY = 5;
-let snakebody = [];
-let velocityX = 0, velocityY = 0;
-let gameOver = false;
+let foodX, foodY; // declaration des variables pour la pomme
+let snakeX = 6, snakeY = 5; // declaration des variables pour le snake
+let snakebody = []; 
+let velocityX = 0, velocityY = 0; // deplacement du snake
+let gameOver = false; 
 let score = 0;
 let setIntervalId ;
 
-let highScore = localStorage.getItem("hScore") || 0;
-highScoreElement.innerText = `High Score: ${highScore}`;
+let highScore = localStorage.getItem("hScore") || 0;    // recuperation de la valeur en local 
+highScoreElement.innerText = `High Score: ${highScore}`; // remplacer le high score html par le js
 
-const changementDirection = (e) => {
+const changementDirection = (e) => {  // fonction pour les changements de direction
     if (e.key === "ArrowUp"){
+        if(velocityY == 1)   // pour empecher de ce déplacer à l'opposer 
+            return;
         velocityX = 0;
-        velocityY = -1;
+        velocityY = -1;   // déplacement vers le haut
     }else if (e.key === "ArrowDown"){
+        if(velocityY == -1) // pour empecher de ce déplacer à l'opposer 
+            return;
         velocityX = 0;
-        velocityY = 1;
+        velocityY = 1; // déplacement vers le bas
     }else if (e.key === "ArrowLeft"){
-        velocityX = -1;
+        if(velocityX == 1) // pour empecher de ce déplacer à l'opposer 
+        return
+        velocityX = -1; // déplacement vers la gauche
         velocityY = 0;
     }else if (e.key === "ArrowRight"){
-        velocityX = 1;
+        if(velocityX == -1) // pour empecher de ce déplacer à l'opposer 
+        return
+        velocityX = 1; // déplacement vers la droite
         velocityY = 0;
     }
 
 }
 
 
-const changeFoodPosition = () => {
+const changeFoodPosition = () => {     //fonction pour faire apparaitre la pomme aléatoirement
 
-    foodX = Math.floor(Math.random()*15)+1;
-    foodY = Math.floor(Math.random()*15)+1;
+    foodX = Math.floor(Math.random()*15)+1; // position sur l'abscisse 
+    foodY = Math.floor(Math.random()*15)+1; // position sur l'ordonné
+    
+    
 }
 
-const handleGameover = () => {
+const handleGameover = () => {   //fonction game over
     clearInterval(setIntervalId);
-    alert("Game Over! Appuyer sur OK pour recommencer");
-    location.reload();
+    alert("Game Over! Appuyer sur OK pour recommencer"); // pop up alerte
+    location.reload(); // recharge de la page
 }
 
 
-const initGame = () => {
+const initGame = () => { // initation de du jeu
 
-    if(gameOver) return  handleGameover();
+    if(gameOver) return  handleGameover();   // si gameover = true donc ça exécute la fontion handleGameover
 
-    let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`; // cela rajoute un div "class food" dans le html
 
     if (snakeX === foodX && snakeY === foodY){
         changeFoodPosition();
@@ -74,7 +84,7 @@ const initGame = () => {
     snakeX += velocityX;
     snakeY += velocityY;
 
-    if(snakeX <= 0 || snakeX >= 16 || snakeY <= 0 || snakeY >= 16){
+    if(snakeX <= 0 || snakeX >=16 || snakeY <= 0 || snakeY >= 16){
         gameOver = true;
     }
     
@@ -89,5 +99,5 @@ const initGame = () => {
 }
 
 changeFoodPosition();
-setIntervalId = setInterval(initGame, 125);
+setIntervalId = setInterval(initGame, 95);
 document.addEventListener("keydown", changementDirection);
